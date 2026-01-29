15d7dd6a315c645e5d2783f2377773139f419eb9
ishubs <shubsgiri@gmail.com>
2026-01-29 14:30:00 +0530

    feat(payments): add formatTxExplorerUrl helper

--- packages/payments/src/index.ts
+++ packages/payments/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 export * from './utils/format';
 export * from './utils/retry';
+export * from './utils/explorer';
 export * from './kyc';
 export * from './client';
 export * as LetsPayV1Abi from './abi/letsPayV1';
